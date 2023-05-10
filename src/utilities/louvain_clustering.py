6efa696495f03468c6e42f7275498469200f4d1b
Tian Tian <tiantian@Tians-MacBook-Pro.local>
2023-05-10 11:39:28 -0400

    add louvain clustering

---
+++ src/utilities/louvain_clustering.py
@@ -0,0 +1,18 @@
+import scanpy as sc
+import numpy as np
+
+n_cluster = None # define the number of clusters here
+
+latent = np.loadtxt("final_latent.txt", delimiter=",")
+adata_latent = sc.AnnData(latent)
+sc.pp.neighbors(adata_latent, n_neighbors=20, use_rep="X")
+res = 0.2
+while True:
+    print("current resolution", res)
+    sc.tl.louvain(adata_latent, resolution=res)
+    y_pred = np.asarray(adata_latent.obs['louvain'], dtype=int)
+    if np.unique(y_pred).shape[0] >= n_cluster:
+        break
+    res += 0.01
+
+np.savetxt("louvain_clustering_labels.txt", y_pred, delimiter=",", fmt="%i")
